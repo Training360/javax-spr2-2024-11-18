88b40f1225a7d19e77972e4128a589e15b026967
vicziani <viczian.istvan@gmail.com>
2024-11-18 15:10:12 +0100

    Scheduled megoldás

---
+++ empapp-scheduled/src/main/java/empapp/CounterController.java
@@ -0,0 +1,27 @@
+package empapp;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/counter")
+@AllArgsConstructor
+@Slf4j
+public class CounterController {
+
+    private final CounterService counterService;
+
+    @PostMapping
+    // https://httpgoats.com/202
+    @ResponseStatus(HttpStatus.ACCEPTED)
+    public void count() {
+        log.info("Proxy class: {}", counterService.getClass().getName());
+        counterService.count();
+    }
+}
