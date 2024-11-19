9d8a4672e552381bab4f3a9a97e57144832872a3
vicziani <viczian.istvan@gmail.com>
2024-11-19 14:27:54 +0100

    WebSocket üzenet fogadás

--- empapp-websocket/src/main/resources/static/js/employees.js
+++ empapp-websocket/src/main/resources/static/js/employees.js
@@ -7,4 +7,15 @@ window.onload = function () {
         const json = JSON.stringify({"requestText": text})
         client.send("/app/messages", {}, json)
     }
+
+    client.connect({}, function (frame) {
+        client.subscribe("/topic/employees", function (message) {
+            const body = message.body
+            const json = JSON.parse(body)
+            const text = json.responseText
+
+            console.log(`Reply: ${text}`)
+            document.querySelector("#message-div").innerHTML += `<p>${text}</p>`
+        })
+    })
 }
