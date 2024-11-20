7561f3f269d59a88b11f5cc5c58d9267eea2debe
vicziani <viczian.istvan@gmail.com>
2024-11-20 12:01:26 +0100

    SOAP kliens

--- empapp-ws-client/src/main/java/empapp/EmpappWsClientApplication.java
+++ empapp-ws-client/src/main/java/empapp/EmpappWsClientApplication.java
@@ -1,13 +1,22 @@
 package empapp;
 
+import https.training360_com.employees.EmployeeWebServiceService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class EmpappWsClientApplication {
+public class EmpappWsClientApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmpappWsClientApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) {
+		var service = new EmployeeWebServiceService();
+		var port = service.getEmployeeWebServicePort();
+		var response = port.findAllEmployees();
+		response.forEach(e -> System.out.println(e.getName()));
+	}
 }
