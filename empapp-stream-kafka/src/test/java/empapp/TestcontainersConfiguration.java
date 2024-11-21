79c43c3782195c8befcaca5362c666eff21f7f4b
vicziani <viczian.istvan@gmail.com>
2024-11-21 09:39:39 +0100

    Kafka

--- empapp-stream-kafka/src/test/java/empapp/TestcontainersConfiguration.java
+++ empapp-stream-kafka/src/test/java/empapp/TestcontainersConfiguration.java
@@ -1,15 +1,21 @@
 package empapp;
 
-import com.github.dockerjava.api.model.Bind;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
 import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Testcontainers;
 import org.testcontainers.utility.DockerImageName;
 
 import java.util.List;
 
 @TestConfiguration(proxyBeanMethods = false)
+@Slf4j
+@Testcontainers
 public class TestcontainersConfiguration {
 
     @Bean
@@ -24,4 +30,14 @@ public class TestcontainersConfiguration {
         container.setPortBindings(List.of("5432:5432"));
         return container;
     }
+
+    @Bean
+    public KafkaContainer kafkaContainer() {
+        KafkaContainer container = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.7.1"))
+                .withKraft()
+                .withReuse(true);
+        container.setPortBindings(List.of("9093:9093"));
+        return container;
+    }
+
 }
